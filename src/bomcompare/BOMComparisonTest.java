7298bc4cf98e0b9630ac318aa3be1787ad85fce3
Travis_Tran <duytantran2710@gmail.com>
2019-04-16 21:49:12 -0700

    Change sample template

--- src/bomcompare/BOMComparisonTest.java
+++ src/bomcompare/BOMComparisonTest.java
@@ -3,7 +3,7 @@ package bomcompare;
 
 public class BOMComparisonTest {
     
-    private static final String TEMPLATE = "E:\\Workspace\\java_projects\\BOMCompare\\BOM_data\\BomComparisonTemplate.xlsx";
+    private static final String TEMPLATE = "E:\\Workspace\\java_projects\\BOMCompare\\BOM_data\\SampleTemplate.xlsx";
     
     public static void main(String[] args) {
         BOMComparison bomComparison = new BOMComparison(TEMPLATE);
